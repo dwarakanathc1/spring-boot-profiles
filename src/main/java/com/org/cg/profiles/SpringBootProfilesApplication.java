5f185477225a7dbb501543d35a3313177548535f
dwarakanathc1 <dwarakanathc1>
2022-10-16 05:16:25 +0100

    first commit

---
+++ src/main/java/com/org/cg/profiles/SpringBootProfilesApplication.java
@@ -0,0 +1,14 @@
+package com.org.cg.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesApplication.class, args);
+	}
+
+}
